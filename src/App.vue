51df421640b8ea7fd5faa9cbba72c65dd1d69b13
Joy PimOn Lakvirote <aweqaweq@hotmail.com>
2021-11-07 23:23:37 +0700

    outlinePimonFoodProject

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-<!--การบ้าน
-   <body>
+  <!------------ การบ้าน ------------>
+  <!--<body>
     <header class="top">
       <a href="#" target="_blank">List</a>
       <a href="#" target="_blank">Home</a>
@@ -11,21 +11,232 @@
       <section class="section2">Section 2</section>
     </section>
   </body> -->
-  <body>
-    <img src="./assets/logo.jpg" alt="Pimon Food" class="logo">
-    <nav class="topNav">
-      <a href="#" rel="noopener noreferrer">อาหาร</a>
-      <a href="#" rel="noopener noreferrer">สมาชิก</a>
-      <a href="#" rel="noopener noreferrer">รายการสั่งซื้อ</a>
-    </nav>
-    <section class="card">
-      <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" alt="ปลากระพง">
-      <article class="des">
-        <p>ปลากะพงทอดราดพริก 3 รส</p>
-        <p>200 baht</p>
-        
-      </article>
-    </section>
+    <body>
+    <div class="main">
+      <!------------ List ------------>
+      <!-- <header class="topLogo">
+        <img src="./assets/logo.jpg" class="logo" />
+      </header>
+      <nav class="topNav">
+        <a href="">อาหาร</a>
+        <a href="">สมาชิก</a>
+        <a href="">รายการสั่งซื้อ</a>
+      </nav>
+      <section class="content">
+        <div class="card1">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card1">
+          <img src="./assets/02ปูเนื้อผัดพริกไทยดำ.jpg" id="foodpicture" />
+          <div>
+            <h4><b>02ปูเนื้อผัดพริกไทยดำ</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+      </section>
+      <section class="content">
+        <div class="card1">
+          <img src="./assets/03กุ้งลายเสือจัมโบ้ราดพริก.jpg" id="foodpicture" />
+          <div>
+            <h4><b>03กุ้งลายเสือจัมโบ้ราดพริก</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card1">
+          <img src="./assets/04ต้มยำกุ้ง น้ำข้น.jpg" id="foodpicture" />
+          <div>
+            <h4><b>04ต้มยำกุ้ง น้ำข้น</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+      </section>
+      <section class="content">
+        <div class="card1">
+          <img
+            src="./assets/05หอยแมลงภู่นิวซีแลนด์อบเนย.jpg" id="foodpicture" />
+          <div>
+            <h4><b>05หอยแมลงภู่นิวซีแลนด์อบเนย</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card1">
+          <img src="./assets/06ปลาหมึกไข่นึ่งมะนาว.jpg" id="foodpicture" />
+          <div>
+            <h4><b>06ปลาหมึกไข่นึ่งมะนาว</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+      </section>
+      <section class="content">
+        <div class="card2">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card2">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card2">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card2">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+      </section>
+      <section class="content">
+        <div class="card2">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card2">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card2">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+        <div class="card2">
+          <img src="./assets/01ปลากะพงทอดราดพริก 3 รส.jpg" id="foodpicture" />
+          <div>
+            <h4><b>01ปลากะพงทอดราดพริก 3 รส</b></h4>
+          </div>
+          <p>200 Bant</p>
+          <button class="button">
+            <img src="./assets/plus.png" id="plus" /> เพิ่ม
+          </button>
+        </div>
+      </section>
+      <footer class="footer">By Pornpimon Lakvirote</footer> -->
+
+      <!------------- register ------------>
+      <!-- <div class="register">
+  <form action="/action_page.php">
+    <label for="fname">Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Tel</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your Tel">
+
+    <label for="country">E-Mail</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your E-Mail">
+
+    <label for="country">Password</label>
+    <input type="text" id="lname" name="lastname" placeholder="Password">
+
+    <label for="country">Re-Password</label>
+    <input type="text" id="lname" name="lastname" placeholder="Re-Password">
+
+    <input type="submit" value="สมัครสมาชิก">
+  </form>
+</div> -->
+
+<!----------- Log-In ------------>
+<!-- <div class="register">
+  <form action="/action_page.php">
+
+    <label for="country">E-Mail</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your E-Mail">
+
+    <label for="country">Password</label>
+    <input type="text" id="lname" name="lastname" placeholder="Password">
+
+    <div class="loginRegister">
+    <input type="submit1" value="Register">
+    <input type="submit1" value="Log-In">
+    </div>
+  </form>
+</div> -->
+
+<div class="profile">
+<div class="privateprofile">
+  <header class="topLogo">
+        <img src="./assets/logo.jpg" class="logo" />
+      </header>
+  <form>
+    <label for="fname">Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Tel</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your Tel">
+
+    <input type="submit" value="แก้ไขข้อมูล">
+  </form>
+</div>
+<div class="listbuy">
+  <div class="listbuy1">รวมเป็นเงิน</div>
+  <div class="listbuy1">รวมเป็นเงิน</div>
+  <div class="listbuy1">รวมเป็นเงิน</div>
+</div>
+</div>
+
+    </div>
   </body>
 </template>
 
@@ -34,43 +245,174 @@ export default {};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Kanit:wght@100&display=swap");
 
 * {
   margin: 0;
   padding: 0;
   list-style: none;
   box-sizing: border-box;
-  font-family: "Roboto", sans-serif;
+  font-family: "Kanit", sans-serif;
+  font-weight: bolder;
 }
-.logo{
+body {
+  background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.7) 0%,rgba(255, 255, 255, 0.7) 100%), url('assets/bg.jpg');
+  background-size: cover;
+  background-attachment: fixed;
+}
+
+.main {
+  width: 80%;
+  margin: 25px auto;
+}
+/* ----------List---------- */
+ .topLogo {
+  text-align: center;
+}
+.logo {
   height: 200px;
   border-radius: 50%;
   border: 10px solid #fff;
 }
-.topNav{
+.topNav {
+  margin: 20px 0;
   padding: 0.25cm 0;
-  margin: 30px 250px 20px;
-  overflow: hidden;
-  background-color: #652929;
+  text-align: center;
 }
-.topNav a{
+.topNav a {
   color: #fbe0df;
   text-align: center;
+  margin: 0 20px;
   padding: 14px 16px;
   text-decoration: none;
   font-size: 17px;
+  border-radius: 10%;
+  background-color: #652929;
 }
-.topNav a:hover{
+.topNav a:hover {
   background-color: #fbe0df;
   color: #652929;
 }
-.topNav a:active{
+.topNav a:active {
   background-color: aqua;
   color: black;
 }
-/*การบ้าน
- .top {
+#plus {
+  width: 5%;
+  height: auto;
+}
+#foodpicture {
+  width: 100%;
+  border-radius: 10px;
+}
+.content {
+  display: flex;
+}
+.card1 {
+  background-color: #fff;
+  width: 50%;
+  border-radius: 10px;
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid;
+}
+.card2 {
+  background-color: #fff;
+  width: 25%;
+  border-radius: 10px;
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid;
+}
+p {
+  font-weight: bold;
+  text-align: right;
+}
+.button {
+  font-size: larger;
+  border-radius: 4px;
+}
+.footer {
+  background-color: #000;
+  padding: 15px 0;
+  color: #fff;
+  text-align: center;
+} 
+/* ----------Register-Login---------- */
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #652929;
+  color: #fff;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  color: #652929;
+  background-color: pink;
+  border: 1px solid #652929;
+}
+
+input[type=submit1] {
+  width: 100%;
+  background-color: #652929;
+  color: #fff;
+  padding: 14px 20px;
+  margin: 8px 8px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  text-align: center;
+}
+
+input[type=submit1]:hover {
+  color: #652929;
+  background-color: pink;
+  border: 5px solid #652929;
+}
+
+.register {
+  border-radius: 5px;
+  padding: 20px;
+}
+.loginRegister{
+  display: flex;
+}
+/* ----------Privave-Profile--------- */
+.profile{
+  display: flex;
+}
+.privateprofile{
+  margin: 0 10px;
+  width: 50%;
+}
+
+.listbuy{
+  margin: 0 10px;
+  width: 50%;
+}
+
+.listbuy1{
+  width: 100%;
+  height: 100px;
+  box-shadow: 0px 0px 10px 0px rgba(50, 50, 50, 0.2);
+  border-radius: 15px;
+}
+/* ----------การบ้าน---------- */
+/* .top {
   background-color: #cdf3c9;
   box-shadow: 0px 0px 30px 0px rgba(50, 50, 50, 0.2);
   padding: 25px 50px;
